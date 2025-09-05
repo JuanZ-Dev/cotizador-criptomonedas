63ecf7d5b447a55ba320a47ce70229c0681b4c84
JuanZ-Dev <juan.z.981611@gmail.com>
2025-09-04 20:30:10 -0500

    create form component and add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import CriptoSearchForm from "./components/CriptoSearchForm";
+
 function App() {
   return (
     <>
-      <h1>Cotizador de Criptomonedas</h1>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+
+        <div className="content">
+          <CriptoSearchForm />
+        </div>
+      </div>
     </>
   );
 }
