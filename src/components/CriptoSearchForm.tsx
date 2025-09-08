367a662675ea9363f2eb7570499bdcb1eaedc874
JuanZ-Dev <juan.z.981611@gmail.com>
2025-09-07 20:20:10 -0500

    display cryptocurrencies in the form

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,9 @@
 import { currencies } from "../data";
+import { useCryptoStore } from "../store";
 
 export default function CriptoSearchForm() {
+  const cryptoCurrencies = useCryptoStore((state) => state.cryptoCurrencies);
+
   return (
     <form className="form">
       <div className="field">
@@ -19,6 +22,11 @@ export default function CriptoSearchForm() {
         <label htmlFor="criptoCurrency">Criptomoneda:</label>
         <select name="criptoCurrency" id="criptoCurrency">
           <option value="">-- Seleccione --</option>
+          {cryptoCurrencies.map((crypto) => (
+            <option key={crypto.CoinInfo.Name} value={crypto.CoinInfo.Name}>
+              {crypto.CoinInfo.FullName}
+            </option>
+          ))}
         </select>
       </div>
 
